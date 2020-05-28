30a7ef9e7460d4c47f41006097e7380b96a00fdf
James231 <james--clements@outlook.com>
2020-05-28 17:54:52 +0100

    update

--- jsinterop.js
+++ jsinterop.js
@@ -1,3 +1,13 @@
-﻿function DisplayAlert(msg) {
+﻿// -------------------------------------------------------------------------------------------------
+// Rest API Template - © Copyright 2020 - Jam-Es.com
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+/*
+  JavaScript functions which can be called from C# code through IJSRuntime
+*/
+
+/* Shows an alert dialog */
+function DisplayAlert(msg) {
     alert(msg);
 }
